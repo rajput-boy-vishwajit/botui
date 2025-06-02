36dca956d3b3bf1e253176aab765fe9baec48f55
rajput-boy-vishwajit <112976792+rajput-boy-vishwajit@users.noreply.github.com>
2025-06-02 19:44:01 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-tf-backend-2025" # Replace with your actual S3 bucket name
+    bucket = "tetris-0987654321" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
